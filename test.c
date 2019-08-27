9a8fe6b632369bb12c59866f674ef73b5d32cd76
lsq1998515 <lsq515@163.com>
2019-08-27 02:56:19 -0700

    test example

---
+++ test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  printf("hello bit \n");  
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  printf("hello bit \n");
+  return 0;
+}
